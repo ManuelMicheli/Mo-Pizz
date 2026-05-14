702043f78a4ea0f7256c94527dc56c3d874c8e67
ManuelMicheli <manumiky03.mm@gmail.com>
2026-05-14 21:53:22 +0200

    Revert "feat(services): swap ServicesGrid for ServicesDrawer overlay; update staff copy"
    This reverts commit 4f68ecf36ad412f4e230adfa6c0384bd5fb8a05e.

--- src/app/ClientGroupA.jsx
+++ src/app/ClientGroupA.jsx
@@ -2,14 +2,14 @@
 
 import dynamic from 'next/dynamic';
 
-const ServicesDrawer = dynamic(() => import('@/components/ServicesDrawer'), { ssr: false });
+const ServicesGrid = dynamic(() => import('@/components/ServicesGrid'));
 const MenuFisso = dynamic(() => import('@/components/MenuFisso'));
 const MenuSection = dynamic(() => import('@/components/menu/MenuSection'), { ssr: false });
 
 export default function ClientGroupA() {
     return (
         <>
-            <ServicesDrawer />
+            <ServicesGrid />
             <MenuFisso />
             <MenuSection />
         </>
